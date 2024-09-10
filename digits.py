fb5decee44050d614d694b566eb8ff3430f31674
Mehri Quluzadə <mehricyber@gmail.com>
2024-09-10 09:16:40 -0400

    Create digits.py

---
+++ digits.py
@@ -0,0 +1,7 @@
+#Bu sualda ise qisaca daxil edeceyimiz ededin nece reqemli oldugunu tapmaq ucun lazim olan programi yaziriq 
+#Meselen:12345 ededi daxil edilir ve cixisa 5 verilir 
+a=input()
+l=list(a)
+print(len(l)
+      #Burada biz ededimiz setr tipinde daxil edib elementleri siyahiya alaraq uzunluq yeni elementlerin sayini tapiriq 
+    
