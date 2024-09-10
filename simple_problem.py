dd75c9da53abcc4976580c55cff955d6c59c297b
Mehri Quluzadə <mehricyber@gmail.com>
2024-09-10 09:12:16 -0400

    Create simple_problem.py

---
+++ simple_problem.py
@@ -0,0 +1,7 @@
+#Bu problem en asand olanlardan birincidir bize ise 10<x<=99 arasinda olan ededleri onluq ve teklik hissesini ayri yan-yana cap etmeyimizi isteyir men ise bunu oz detaylandirmam ile edecem :)))
+x=int(input())
+if x>9 and x<100:
+  print(x//10,x%10)
+else:
+  print("Daxil etdiyiniz eded serti odemir")
+  
