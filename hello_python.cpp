f353fb61ff18922584b8c7434b2810bdb78aa9f6
Mehri Quluzadə <mehricyber@gmail.com>
2024-09-13 13:54:18 -0400

    Create hello_python.cpp

---
+++ hello_python.cpp
@@ -0,0 +1,6 @@
+  //Bize burada sadece Hello Python sozun cixisa vermeyimiz istenilir men ise bunu hec olmaya c++ da edim qerarina geldim
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Hello, Python!";
+}
